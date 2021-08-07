08c0cf4dce96c7abc84b07a23d59cfa5d9d8b129
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2021-08-07 18:38:10 +0530

    fetching user input

--- bmi.go
+++ bmi.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Output information
+	fmt.Println("BMI Calculator")
+	fmt.Println("--------------------")
+	// Prompt for user input (weight + height)
+	fmt.Print("Please enter your weight in (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Please enter your height in (m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	fmt.Print(weightInput)
+	fmt.Print(heightInput)
+	// Save that user input in variables
+	// Calculate BMI (weight / height * height)
+	// Output the calculated BMI
 }
