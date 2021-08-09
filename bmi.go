402c2759504eb35502555525c4939ebde3cc26ba
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2021-08-09 21:07:53 +0530

    outsourcing the input logic

--- bmi.go
+++ bmi.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/dpthegrey/upgraded-winner/info"
 )
@@ -11,22 +9,8 @@ import (
 func main() {
 	// Output information
 	info.PrintWelcome()
-	// Prompt for user input (weight + height)
-	fmt.Print(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
 
-	fmt.Print(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
-
-	// Save that user input in variables
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-
-	fmt.Print(weightInput)
-	fmt.Print(heightInput)
-
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
+	weight, height := getUserMetrics()
 
 	// Calculate BMI (weight / height * height)
 	bmi := weight / (height * height)
